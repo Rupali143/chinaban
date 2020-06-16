1743972a2abbd6a53e50f331757f805e05613dc1
Neosoft <neha.bhole@neosofttech.com>
2020-06-16 12:17:22 +0530

    captcha vendor file added

--- app/Http/Controllers/LoginController.php
+++ app/Http/Controllers/LoginController.php
@@ -70,7 +70,7 @@ class LoginController extends Controller
             } 
         }catch(\Exception $ex){
             return redirect()->route('admin.login')->with('error',$ex->getMessage);
-        }           
+        } 
     }
 
     /**
