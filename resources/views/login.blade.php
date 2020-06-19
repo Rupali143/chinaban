2611df9ad3aa6de52558ca5f70ff4ccba34d465a
Neosoft <neha.bhole@neosofttech.com>
2020-06-19 22:48:46 +0530

    add formatting in product and login page

--- resources/views/login.blade.php
+++ resources/views/login.blade.php
@@ -1,92 +1,88 @@
 <!DOCTYPE html>
 <html>
-<head>
-  <meta charset="utf-8">
-  <meta http-equiv="X-UA-Compatible" content="IE=edge">
-  <title>Log in</title>
-  <!-- Tell the browser to be responsive to screen width -->
-  <meta name="viewport" content="width=device-width, initial-scale=1">
-  <!-- Font Awesome -->
-  <link rel="stylesheet" href="{{ asset('plugins/fontawesome-free/css/all.min.css') }}">
-  <!-- Ionicons -->
-  <link rel="stylesheet" href="https://code.ionicframework.com/ionicons/2.0.1/css/ionicons.min.css">
-  <!-- icheck bootstrap -->
-  <link rel="stylesheet" href="{{ asset('plugins/icheck-bootstrap/icheck-bootstrap.min.css') }}">
-  <!-- Theme style -->
-  <link rel="stylesheet" href="{{ asset('dist/css/adminlte.min.css') }}">
-  <link rel="stylesheet" href="{{ asset('css/customize.css') }}">
-  <!-- Google Font: Source Sans Pro -->
-  <link href="https://fonts.googleapis.com/css?family=Source+Sans+Pro:300,400,400i,700" rel="stylesheet">
-</head>
-<body class="hold-transition login-page">
-<div class="login-box">
-
-  @if( Session::has( 'error' ))
-  <div class="alert alert-danger" id="errorMessage">
-    <span class="glyphicon glyphicon-ok">{{ Session::get( 'error' ) }}</span>
-  </div>
-  @endif
-  
-  <div class="login-logo">
-    <a href="#"><b>Login</a>
-  </div>
-  <!-- /.login-logo -->
-  <div class="card">
-    <div class="card-body login-card-body">
-      <form role="form" method="post" action="{{route('login')}}" id="loginForm">
-        @csrf
-        <div class="input-group mb-3">
-          <input type="username" name ="username" class="form-control" placeholder="Username">
-          <div class="input-group-append">
-            <div class="input-group-text">
-              <span class="fas fa-envelope"></span>
-            </div>
-          </div>
-        </div>
-        
-        @error('username')
-          <span class="text-danger errormsg" role="alert">
-           <p>{{ $message }}</p>
-          </span>
-        @enderror
-        <div class="input-group mb-3">
-          <input type="password" name="password" class="form-control" placeholder="Password">
-          <div class="input-group-append">
-            <div class="input-group-text">
-              <span class="fas fa-lock"></span>
-            </div>
-          </div>
-        </div>
-        @error('password')
-          <span class="text-danger error errormsg" role="alert">
-          <p>{{ $message }}</p>
-          </span>
-        @enderror
-        <div class="input-group mb-3">
-        @captcha
-          <input type="text" id="captcha" style="border-radius:5px" class="form-controll" name="captcha" autocomplete="off">
-          @error('captcha')
-          <span class="text-danger errormsg" role="alert">
-           <p>{{ $message }}</p>
-          </span>
-         @enderror
-        </div>
-        <div class="row">
-          <div class="col-4">
-          </div>
-          <div class="col-4">
-            <button type="submit" class="btn btn-primary btn-block">Sign In</button>
-          </div>
-          <div class="col-4">
-          </div>
-          <!-- /.col -->
-        </div>
-      </form>
-      <!-- /.login-card-body -->
-  </div>
-</div>
-<!-- /.login-box -->
-
+   <head>
+      <meta charset="utf-8">
+      <meta http-equiv="X-UA-Compatible" content="IE=edge">
+      <title>Log in</title>
+      <!-- Tell the browser to be responsive to screen width -->
+      <meta name="viewport" content="width=device-width, initial-scale=1">
+      <!-- Font Awesome -->
+      <link rel="stylesheet" href="{{ asset('plugins/fontawesome-free/css/all.min.css') }}">
+      <!-- Ionicons -->
+      <link rel="stylesheet" href="https://code.ionicframework.com/ionicons/2.0.1/css/ionicons.min.css">
+      <!-- icheck bootstrap -->
+      <link rel="stylesheet" href="{{ asset('plugins/icheck-bootstrap/icheck-bootstrap.min.css') }}">
+      <!-- Theme style -->
+      <link rel="stylesheet" href="{{ asset('dist/css/adminlte.min.css') }}">
+      <link rel="stylesheet" href="{{ asset('css/customize.css') }}">
+      <!-- Google Font: Source Sans Pro -->
+      <link href="https://fonts.googleapis.com/css?family=Source+Sans+Pro:300,400,400i,700" rel="stylesheet">
+   </head>
+   <body class="hold-transition login-page">
+      <div class="login-box">
+      @if( Session::has( 'error' ))
+      <div class="alert alert-danger" id="errorMessage">
+         <span class="glyphicon glyphicon-ok">{{ Session::get( 'error' ) }}</span>
+      </div>
+      @endif
+      <div class="login-logo">
+         <a href="#"><b>Login</a>
+      </div>
+      <!-- /.login-logo -->
+      <div class="card">
+         <div class="card-body login-card-body">
+            <form role="form" method="post" action="{{route('login')}}" id="loginForm">
+               @csrf
+               <div class="input-group mb-3">
+                  <input type="username" name ="username" class="form-control" placeholder="Username">
+                  <div class="input-group-append">
+                     <div class="input-group-text">
+                        <span class="fas fa-envelope"></span>
+                     </div>
+                  </div>
+               </div>
+               @error('username')
+               <span class="text-danger errormsg" role="alert">
+                  <p>{{ $message }}</p>
+               </span>
+               @enderror
+               <div class="input-group mb-3">
+                  <input type="password" name="password" class="form-control" placeholder="Password">
+                  <div class="input-group-append">
+                     <div class="input-group-text">
+                        <span class="fas fa-lock"></span>
+                     </div>
+                  </div>
+               </div>
+               @error('password')
+               <span class="text-danger error errormsg" role="alert">
+                  <p>{{ $message }}</p>
+               </span>
+               @enderror
+               <div class="input-group mb-3">
+                  @captcha
+                  <input type="text" id="captcha" style="border-radius:5px" class="form-controll" name="captcha" autocomplete="off">
+                  @error('captcha')
+                  <span class="text-danger errormsg" role="alert">
+                     <p>{{ $message }}</p>
+                  </span>
+                  @enderror
+               </div>
+               <div class="row">
+                  <div class="col-4">
+                  </div>
+                  <div class="col-4">
+                     <button type="submit" class="btn btn-primary btn-block">Sign In</button>
+                  </div>
+                  <div class="col-4">
+                  </div>
+                  <!-- /.col -->
+               </div>
+            </form>
+            <!-- /.login-card-body -->
+         </div>
+      </div>
+      <!-- /.login-box -->
 <script src="{{ asset('plugins/jquery/jquery.min.js') }}"></script>
 <script src="{{ asset('plugins/bootstrap/js/bootstrap.bundle.min.js') }}"></script>
 <script src="{{ asset('plugins/jquery-validation/jquery.validate.min.js') }}"></script>
